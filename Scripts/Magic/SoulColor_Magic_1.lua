2140cfefdda30c711780ef632666cea168e7caa1
1500665289 <152504086+1500665289@users.noreply.github.com>
2025-12-09 23:43:17 +0800

    更新 SoulColor_Magic_1.lua

--- Scripts/Magic/SoulColor_Magic_1.lua
+++ Scripts/Magic/SoulColor_Magic_1.lua
@@ -1,59 +1,75 @@
 --炼制灵石
-local tbTable = GameMain:GetMod("MagicHelper");--获取神通模块 这里不要动
-local tbMagic = tbTable:GetMagic("SoulColor_Magic_1");--创建一个新的神通class
+local tbTable = GameMain:GetMod("MagicHelper")  --获取神通模块
+local tbMagic = tbTable:GetMagic("LingStoneMake")  --修改为XML中的魔法名称
 
-local Count;
-
---注意-
---神通脚本运行的时候有两个固定变量
---self.bind 执行神通的npcObj
---self.magic 当前神通的数据，也就是定义在xml里的数据
+local frameCount  -- 帧计数器
+local totalFrames  -- 总帧数
 
 function tbMagic:Init()
+    -- 初始化代码
 end
 
 --神通是否可用
 function tbMagic:EnableCheck(npc)
-	return true;
+    -- 检查是否有足够灵力
+    if npc.LingV >= 5000 then
+        return true
+    end
+    return false
 end
 
---目标合法检测 首先会通过magic的SelectTarget过滤，然后再通过这里过滤
---IDs是一个List<int> 如果目标是非对象，里面的值就是地点key，如果目标是物体，值就是对象ID，否则为nil
---IsThing 目标类型是否为物体
+--目标合法检测
 function tbMagic:TargetCheck(key, t)
-	return true
+    return true
 end
 
 --开始施展神通
 function tbMagic:MagicEnter(IDs, IsThing)
-	self.Count = 0;
-	-- 移除增加的灵力操作，改为在施法过程中检查灵力是否足够
+    frameCount = 0
+    -- XML中Param1=2，表示2单位时间
+    -- 假设1单位时间=10帧
+    totalFrames = 2 * 10
 end
 
---神通施展过程中，需要返回值
---返回值  0继续 1成功并结束 -1失败并结束
-function tbMagic:MagicStep(dt,duration)
-	--self:SetProgress(durationf.magic.Param1);--设置施展进度 主要用于UI显示 这里使用了参数1作为施法时间
-	self.Count = self.Count + 1;
-	if self.Count == 150 then
-		-- 检查灵力是否足够
-		if self.bind.LingV >= self.magic.CostLing then
-			local item = CS.XiaWorld.ItemRandomMachine.RandomItem("Item_LingStone");--读取物品
-			item.FSItemState = -1;--镇物状态0未知 -1无 1有未鉴定 2有已鉴定
-			self.bind.map:DropItem(item,self.bind.Key,true,true,false,false,0,false);--地图掉落物品方法：物品、地点、是否可见、是否携带、没有自我、需要点击、等待、分散。
-			print(self.bind.LingV);
-			print(self.bind);
-			self.bind:AddLing(-self.magic.CostLing);--消耗灵力
-			self.Count = 0;
-			return 0;--继续施法
-		else
-			-- 灵力不足，结束施法
-			return 1;
-		end
-	end
-	return 0;
+--神通施展过程中
+function tbMagic:MagicStep(dt, duration)
+    frameCount = frameCount + 1
+    
+    -- 更新进度条显示
+    self:SetProgress(frameCount / totalFrames)
+    
+    if frameCount >= totalFrames then
+        -- 施法完成，再次检查灵力
+        if self.bind.LingV >= 5000 then
+            -- 生成灵石
+            local item = CS.XiaWorld.ItemRandomMachine.RandomItem("Item_LingStone")
+            item.FSItemState = -1
+            
+            -- 在地图上掉落物品
+            self.bind.map:DropItem(item, self.bind.Key, true, true, false, false, 0, false)
+            
+            -- 消耗灵力
+            self.bind:AddLing(-5000)
+            
+            print(string.format("%s 炼制灵石成功，消耗5000灵力，剩余%.0f灵力", 
+                self.bind.Name, self.bind.LingV))
+            
+            return 1  -- 成功并结束
+        else
+            print(string.format("%s 灵力不足，炼制灵石失败", self.bind.Name))
+            return -1  -- 失败并结束
+        end
+    end
+    
+    return 0  -- 继续施法
 end
 
---施展完成/失败 success是否成功
+--施展完成/失败
 function tbMagic:MagicLeave(success)
+    frameCount = 0
+    if success then
+        print("炼制灵石成功完成")
+    else
+        print("炼制灵石被中断")
+    end
 end
