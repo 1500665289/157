cc9e83f74c923b08cb2c9bcd6d8a8eae06088bb7
1500665289 <152504086+1500665289@users.noreply.github.com>
2025-12-10 00:07:56 +0800

    更新 SoulColor_Magic_1.lua

--- Scripts/Magic/SoulColor_Magic_1.lua
+++ Scripts/Magic/SoulColor_Magic_1.lua
@@ -3,7 +3,7 @@ local tbTable = GameMain:GetMod("MagicHelper")
 local tbMagic = tbTable:GetMagic("SoulColor_Magic_1")
 
 -- 常量定义
-local GENERATION_INTERVAL = 150     -- 灵石生成间隔帧数
+local GENERATION_INTERVAL = 15     -- 灵石生成间隔帧数
 local MIN_LING_REQUIRED = 0         -- 最小灵气需求
 
 -- 内部状态枚举
